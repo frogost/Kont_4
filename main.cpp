38b5404276ef409299191959efa727e3c606085a
crankocial <kirill201098@mail.ru>
2018-12-21 12:48:15 +0300

    Ex2 & Ex3

--- main.cpp
+++ main.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
+#include "math.h"
 
 class Construct {
     int* x;
     float* y;
 
 public:
-    int *getX() const {
-        return x;
+    int getX() const {
+        return *x;
     }
 
     void setX(int *x) {
         Construct::x = x;
     }
 
-    float *getY() const {
-        return y;
+    float getY() const {
+        return *y;
     }
 
     void setY(float *y) {
@@ -26,8 +27,8 @@ public:
         y = new float;
         *x=_x;
         *y=_y;
-        std::cout<<*x<<'\n';
-        std::cout<<*y<<'\n';
+        std::cout<<_x<<'\n';
+        std::cout<<_y<<'\n';
         std::cout << "Конструктор\n";
     }
 
@@ -37,10 +38,24 @@ public:
         std::cout << "Деструктор\n";
     }
 
+    void change (){
+        *y=pow(*x,2);
+        while (*y>1){
+            *y=*y/10;
+        }
+        *y=*y+*x;
+    }
 };
 
+    bool more (Construct* a,Construct* b){
+        return a->getX()==b->getX();
+    }
+
 int main() {
-    Construct a(4,5);
+    Construct a(21,5);
+    Construct b(5,6);
+    a.change();
+    std::cout<<more(&a,&b)<<'\n';
     std::cout<<a.getX()<<'\n';
     std::cout<<a.getY()<<'\n';
     return 0;
